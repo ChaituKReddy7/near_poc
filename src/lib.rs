45e188c5f140af5082d5bd984134f4805a3da741
ChaituKReddy7 <105509734+ChaituKReddy7@users.noreply.github.com>
2022-08-13 20:04:01 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,48 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::collections::LookupMap;
+use near_sdk::{env, log, near_bindgen, AccountId};
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Counter {
-    val: u8,
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Status {
+    records: LookupMap<AccountId, String>,
+}
+
+impl Default for Status {
+    fn default() -> Self {
+        Self {
+            records: LookupMap::new(b"r".to_vec()),
+        }
+    }
 }
 
 #[near_bindgen]
-impl Counter {
-    pub fn get_value(&self) -> u8 {
-        self.val
-    }
-
-    pub fn increment(&mut self) {
-        self.val += 1;
-        log!("The incremented value is {}", self.val);
-    }
-
-    pub fn decrement(&mut self) {
-        if self.val > 0 {
-            self.val -= 1;
+impl Status {
+    pub fn update(&mut self, message: String) {
+        let account_id = env::signer_account_id();
+        let exists = self.records.contains_key(&account_id);
+        if exists {
+            self.records.insert(&account_id, &message);
         } else {
-            log!("Cannot go below zero");
+            log!("Key doesn't exist");
         }
     }
 
-    pub fn reset(&mut self) {
-        self.val = 0;
+    pub fn create(&mut self, message: String) {
+        let account_id = env::signer_account_id();
+        self.records.insert(&account_id, &message);
+        log!(
+            "The message stored for the account {}, is {}",
+            account_id,
+            message
+        );
+    }
+
+    pub fn get(&self, account_id: AccountId) -> String {
+        let value = match self.records.get(&account_id) {
+            Some(message) => message,
+            None => String::from("No value"),
+        };
+        value
     }
 }
